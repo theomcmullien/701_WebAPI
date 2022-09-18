9a39a628c45b8d03c9e4381cc080c2e7ee18b2f8
Theo McMullien <theomcmullien.16@gmail.com>
2022-09-19 04:51:27 +1200

    Started implementing auth0

---
+++ 701_WebAPI/Models/JWT/HasScopeHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace _701_WebAPI.Models.JWT
+{
+    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
+                return Task.CompletedTask;
+
+            var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer).Value.Split(' ');
+
+            if (scopes.Any(s => s == requirement.Scope))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- 701_WebAPI/Program.cs
+++ 701_WebAPI/Program.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using _701_WebAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using _701_WebAPI.Models.JWT;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +29,30 @@ builder.Services.AddCors(options =>
     });
 }); //Cors
 
+string domain = "https://dev-a3wdzleo.us.auth0.com/";
+string identifier = "https://701-WebAPI-Auth0/api"; //change in appsettings.json
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = domain;
+    options.Audience = identifier;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        NameClaimType = ClaimTypes.NameIdentifier
+    };
+}); //Jwt
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("read:everything", policy => policy.Requirements.Add(new HasScopeRequirement("read:everything", domain)));
+}); //Jwt
+
+builder.Services.AddSingleton<IAuthorizationHandler, HasScopeHandler>(); //Jwt
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -36,6 +65,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors(); //Cors
+
+app.UseAuthentication(); //Jwt
 
 app.UseAuthorization();
 
